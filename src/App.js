6868078bdab1fdc8716ed1688e037256dc45d42e
Ranjan-17 <ranjancrt@gmail.com>
2023-12-21 15:40:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import Nav from './Nav';
 import Home from './Home';
 import NewPost from './NewPost';
 import PostPage from './PostPage';
-import Post from './Post';
 import About from './About';
 import Missing from './Missing';
-import { Routes,Route,Link, useSearchParams, useNavigate} from 'react-router-dom'
-import PostLayout from './PostLayout';
+import { Routes,Route,useNavigate} from 'react-router-dom'
 import { useEffect, useState } from 'react';
 import './index.css'
 import { format } from 'date-fns';
 import api from './api/posts';
 import EditPost from './EditPost';
-// import { DataProvider } from './Context/DataContext';
 import useAxiosFetch from './hooks/useAxiosFetch';
 
 function App() {
@@ -31,10 +28,7 @@ function App() {
 
  useEffect(()=>{
   setPosts(data);
- },[data])
- 
-
-  
+ },[data])  
 
   useEffect(()=>{
     const filteredresults=posts.filter((post)=>((post.body).toLowerCase()).includes(search.toLowerCase())
@@ -76,7 +70,7 @@ function App() {
     const updatedPost ={id,title:editTitle,dateTime,body:editBody};
     try{
       const response =await api.put(`/posts/${id}`,updatedPost);
-      setPosts(posts.map(post=>post.id==id?{...response.data}:post));
+      setPosts(posts.map(post=>post.id===id?{...response.data}:post));
       setPostTitle('');
       setPostBody('');
       navigate('/');
@@ -88,29 +82,7 @@ function App() {
   
   
   return (
-    <div className="App">
-
-
-      {/* <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-         
-          <li><Link to="/postpage">PostPage</Link></li>
-        </ul>
-      </nav> */}
-        {/* <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/postpage" element={<PostLayout/>} >    
-              <Route index element={<PostPage/>}/>
-              <Route path=":id" element={<Post/>}/>
-              <Route path="newpost" element={<NewPost/>} />
-          </Route>
-              <Route path="*" element={<Missing/>}/>
-         
-        </Routes> */}
-       {/* <DataProvider> */}
+    <div className="App">    
             <Header title="RANJAN SOCIAL MEDIA"/>
             <Nav search={search}
             setSearch={setSearch}/>
@@ -142,7 +114,7 @@ function App() {
                   <Route path='*' element={<Missing/>}/>
             </Routes>
             <Footer/>
-       {/* </DataProvider> */}
+      
     </div>
   );
 }
